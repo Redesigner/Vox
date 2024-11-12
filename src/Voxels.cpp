abb76ca8aa2446ea378f78a4d0b9313f8c463fb2
steph <stephenwmelnick@gmail.com>
2024-11-12 11:31:47 -0500

    Initial commit

---
+++ src/Voxels.cpp
@@ -0,0 +1,32 @@
+﻿#include "Voxels.h"
+
+#include "raylib.h"
+
+#include "imgui.h"
+#include "rlImGui.h"
+
+int main()
+{
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 450, "raylib [core] example - basic window");
+    rlImGuiSetup(true);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        // start ImGui Conent
+        rlImGuiBegin();
+
+        // show ImGui Content
+        bool open = true;
+        ImGui::ShowDemoWindow(&open);
+        rlImGuiEnd();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
