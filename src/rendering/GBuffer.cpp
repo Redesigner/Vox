d803c63bdd7dfbcca09946475e89a7d0286a58c6
steph <stephenwmelnick@gmail.com>
2024-11-14 12:11:42 -0500

    added gbuffer

--- src/rendering/GBuffer.cpp
+++ src/rendering/GBuffer.cpp
@@ -7,18 +7,24 @@ GBuffer::GBuffer(int width, int height)
 	:width(width), height(height)
 {
 	TraceLog(LOG_INFO, "Allocating GBuffer");
+
 	framebuffer = rlLoadFramebuffer();
+	if (!framebuffer)
+	{
+		TraceLog(LOG_WARNING, "Failed to create framebuffer");
+	}
+
 	rlEnableFramebuffer(framebuffer);
 
 	positionTexture = rlLoadTexture(NULL, width, height, RL_PIXELFORMAT_UNCOMPRESSED_R32G32B32, 1);
 	normalTexture = rlLoadTexture(NULL, width, height, RL_PIXELFORMAT_UNCOMPRESSED_R32G32B32, 1);
 	albedoSpecTexture = rlLoadTexture(NULL, width, height, RL_PIXELFORMAT_UNCOMPRESSED_R8G8B8A8, 1);
 
+	rlActiveDrawBuffers(3);
+
 	rlFramebufferAttach(framebuffer, positionTexture, RL_ATTACHMENT_COLOR_CHANNEL0, RL_ATTACHMENT_TEXTURE2D, 0);
 	rlFramebufferAttach(framebuffer, normalTexture, RL_ATTACHMENT_COLOR_CHANNEL1, RL_ATTACHMENT_TEXTURE2D, 0);
 	rlFramebufferAttach(framebuffer, albedoSpecTexture, RL_ATTACHMENT_COLOR_CHANNEL2, RL_ATTACHMENT_TEXTURE2D, 0);
-
-	rlActiveDrawBuffers(3);
 
 	depthRenderbuffer = rlLoadTextureDepth(width, height, true);
 	rlFramebufferAttach(framebuffer, depthRenderbuffer, RL_ATTACHMENT_DEPTH, RL_ATTACHMENT_RENDERBUFFER, 0);
@@ -37,4 +43,9 @@ GBuffer::~GBuffer()
 	rlUnloadTexture(normalTexture);
 	rlUnloadTexture(albedoSpecTexture);
 	rlUnloadTexture(depthRenderbuffer);
+}
+
+void GBuffer::EnableFramebuffer()
+{
+	rlEnableFramebuffer(framebuffer);
 }
