f8bf6b00c8843a37fafe8791caef6e11cb4f617a
steph <stephenwmelnick@gmail.com>
2024-11-22 17:00:20 -0500

    Added base octree class

---
+++ src/voxel/Octree.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <array>
+
+struct Voxel;
+
+namespace Octree
+{
+	class Node
+	{
+		enum State : char
+		{
+			Full,
+			Empty,
+			Partial
+		};
+
+	public:
+		Node(unsigned int size);
+		Node(unsigned int size, Voxel* voxel);
+		~Node();
+
+		Voxel* GetVoxel(int x, int y, int z);
+		void SetVoxel(int x, int y, int z, Voxel* voxel);
+
+	private:
+		static int GetIndex(int x, int y, int z);
+		void* GetAccessor(int x, int y, int z) const;
+
+		void CollapseSubnodes(Voxel* voxel);
+
+		std::array<void*, 8> subNodes;
+
+		State state;
+		unsigned short size;
+	};
+}
---
+++ src/voxel/Vector.cpp
@@ -0,0 +1,21 @@
+#include "Vector.h"
+
+Vector3i::Vector3i()
+	:x(0), y(0), z(0)
+{
+}
+
+Vector3i::Vector3i(int x, int y, int z)
+	:x(x), y(y), z(z)
+{
+}
+
+Vector3iu::Vector3iu()
+	:x(0), y(0), z(0)
+{
+}
+
+Vector3iu::Vector3iu(unsigned int x, unsigned int y, unsigned int z)
+	:x(x), y(y), z(z)
+{
+}
