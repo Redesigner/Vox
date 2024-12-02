0af25c3925d47d016604398b8181cabb5154333a
steph <stephenwmelnick@gmail.com>
2024-12-02 16:46:21 -0500

    Added Contact Listener

---
+++ src/physics/ContactListener.cpp
@@ -0,0 +1,23 @@
+#include "ContactListener.h"
+
+#include "raylib.h"
+
+JPH::ValidateResult	ContactListener::OnContactValidate(const JPH::Body& inBody1, const JPH::Body& inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult& inCollisionResult)
+{
+	return JPH::ValidateResult::AcceptAllContactsForThisBodyPair;
+}
+
+void ContactListener::OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings)
+{
+	TraceLog(LOG_INFO, "Contact added.");
+}
+
+void ContactListener::OnContactPersisted(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings)
+{
+	TraceLog(LOG_INFO, "Contact persisted.");
+}
+
+void ContactListener::OnContactRemoved(const JPH::SubShapeIDPair& inSubShapePair)
+{
+	TraceLog(LOG_INFO, "Contact ended.");
+}
