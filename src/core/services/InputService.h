3f92a7229eacc24c2eb262da7d1fe540892b444e
steph <stephenwmelnick@gmail.com>
2024-12-08 14:38:30 -0500

    Moved input events directly to SDL

--- src/core/services/InputService.h
+++ src/core/services/InputService.h
@@ -23,11 +23,17 @@ namespace Vox
 
 		void UnregisterCallback(SDL_Scancode scancode, KeyboardEventCallback callback);
 
+		bool ShouldCloseWindow() const;
+
 	private:
 		void HandleEvent(SDL_Event* event);
+
+		void HandleWindowEvent(SDL_WindowEvent& windowEvent);
 
 		void ExecuteCallbacks(SDL_Scancode scancode, bool pressed);
 
 		std::unordered_map<SDL_Scancode, std::vector<KeyboardEventCallback>> keyboardEventMap;
+
+		bool windowClosed = false;
 	};
 }
