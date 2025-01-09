884bb36007b516e179e5899b5f8ca7c7effd3a67
steph <stephenwmelnick@gmail.com>
2025-01-09 17:22:08 -0500

    Added opengl shader compilation

--- src/rendering/shaders/Shader.cpp
+++ src/rendering/shaders/Shader.cpp
@@ -1,9 +1,53 @@
 #include "Shader.h"
 
 #include <GL/glew.h>
+#include <SDL3/SDL_iostream.h>
+
+#include "core/logging/Logging.h"
 
 namespace Vox
 {
+	bool Shader::Load(std::string vertexShaderFilePath, std::string fragmentShaderFilePath)
+	{
+		shader = glCreateProgram();
+
+		std::optional<unsigned int> vertexShaderId = LoadShaderStage(vertexShaderFilePath, GL_VERTEX_SHADER);
+		if (!vertexShaderId)
+		{
+			glDeleteProgram(shader);
+			return false;
+		}
+
+		std::optional<unsigned int> fragmentShaderId = LoadShaderStage(fragmentShaderFilePath, GL_FRAGMENT_SHADER);
+		if (!fragmentShaderId)
+		{
+			glDeleteProgram(shader);
+			return false;
+		}
+
+		glAttachShader(shader, vertexShaderId.value());
+		glAttachShader(shader, fragmentShaderId.value());
+		glLinkProgram(shader);
+
+		GLint isLinked = 0;
+		glGetProgramiv(shader, GL_LINK_STATUS, &isLinked);
+		if (isLinked == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetProgramiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetProgramInfoLog(shader, maxLength, &maxLength, &infoLog[0]);
+			VoxLog(Error, Rendering, "Failed to link shader: {}", infoLog);
+
+			glDeleteProgram(shader);
+			return false;
+		}
+
+		VoxLog(Display, Rendering, "Shader created successfully.");
+		return true;
+	}
+
 	void Shader::Enable()
 	{
 		glUseProgram(shader);
@@ -42,5 +86,45 @@ namespace Vox
 	void Shader::SetUniformMatrix(int uniformLocation, glm::mat4x4 matrix)
 	{
 		glUniformMatrix4fv(uniformLocation, 1, false, &matrix[0][0]);
+	}
+
+	std::optional<unsigned int> Shader::LoadShaderStage(std::string shaderFilePath, unsigned int shaderType)
+	{
+		SDL_IOStream* shaderIO = SDL_IOFromFile(shaderFilePath.c_str(), "r");
+		if (!shaderIO)
+		{
+			VoxLog(Error, FileSystem, "Failed to load shader file. SDL: {}", SDL_GetError());
+			return std::nullopt;
+		}
+
+		void* shaderSource = SDL_LoadFile_IO(shaderIO, nullptr, true);
+		if (!shaderSource)
+		{
+			VoxLog(Error, FileSystem, "Failed to load shader file. SDL: {}", SDL_GetError());
+			return std::nullopt;
+		}
+
+		char* shaderString = static_cast<char*>(shaderSource);
+
+		unsigned int shaderId = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(shaderId, 1, &shaderString, 0);
+		glCompileShader(shaderId);
+
+		GLint shaderCompilationResult = GL_FALSE;
+		glGetShaderiv(shaderId, GL_COMPILE_STATUS, &shaderCompilationResult);
+		if (shaderCompilationResult == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> errorLog(maxLength);
+			glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
+			VoxLog(Error, Rendering, "Shader failed to compile: {}", errorLog);
+
+			glDeleteShader(shaderId);
+			return std::nullopt;
+		}
+
+		return shaderId;
 	}
 }
