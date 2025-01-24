1ab69feaaa9c38439da07f8e0a76760f10dbcb85
steph <stephenwmelnick@gmail.com>
2025-01-24 13:49:22 -0500

    Began working on voxel physics component, added list ref type

--- src/rendering/VoxelMesh.h
+++ src/rendering/VoxelMesh.h
@@ -5,6 +5,7 @@
 
 #include <glm/glm.hpp>
 
+#include "core/datatypes/Ref.h"
 #include "voxel/Voxel.h"
 
 namespace Vox
@@ -31,25 +32,5 @@ namespace Vox
 		glm::mat4x4 transform;
 
 		unsigned int voxelDataSsbo, voxelMeshSsbo, meshCounter;
-	};
-
-	// @TODO: make a templated version of this class for vectors?
-	// What happens if our reference is invalidated?
-	struct VoxelMeshRef
-	{
-	public:
-		VoxelMeshRef();
-
-		VoxelMeshRef(std::vector<VoxelMesh>* container, size_t index);
-
-		VoxelMesh* operator->();
-
-		VoxelMesh& operator*();
-
-		const VoxelMesh& operator*() const;
-
-	private:
-		std::vector<VoxelMesh>* container;
-		size_t index;
 	};
 }
