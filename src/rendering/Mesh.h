7c3a92ee4320b1b69d9c31a99db1b54721fb886e
steph <stephenwmelnick@gmail.com>
2025-01-29 14:38:22 -0500

    Mesh data uploads correctly from GLTF

--- src/rendering/Mesh.h
+++ src/rendering/Mesh.h
@@ -4,12 +4,27 @@
 
 namespace Vox
 {
+	// @TODO: consider renaming to primtive to match GLTF naming conventions
 	class Mesh
 	{
 	public:
-		Mesh(tinygltf::Model& meshSource, unsigned int meshIndex);
+		Mesh(unsigned int vertexCount, unsigned int componentType, unsigned int indexBuffer, unsigned int positionBuffer, unsigned int normalBuffer, unsigned int uvBuffer);
+
+		unsigned int GetVertexCount() const;
+
+		unsigned int GetComponentType() const;
+
+		unsigned int GetIndexBuffer() const;
+
+		unsigned int GetPositionBuffer() const;
+
+		unsigned int GetNormalBuffer() const;
+
+		unsigned int GetUVBuffer() const;
 
 	private:
-
+		unsigned int vertexCount;
+		unsigned int componentType;
+		unsigned int indexBuffer, positionBuffer, normalBuffer, uvBuffer;
 	};
 }
